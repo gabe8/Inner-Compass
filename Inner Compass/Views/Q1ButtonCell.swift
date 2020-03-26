baeea8fd5272ea3ef1d21267aa8c7ac4aa041dfd
Gabriel Sereni <gabe.sereni@gmail.com>
2020-03-25 21:39:54 -0600

    Saving in UserDefaults Working, Reload of Q1 Almost Complete

---
+++ Inner Compass/Views/Q1ButtonCell.swift
@@ -0,0 +1,24 @@
+//
+//  Q1ButtonCell.swift
+//  Inner Compass
+//
+//  Created by Gabriel Sereni on 3/6/20.
+//  Copyright © 2020 Gabriel Sereni. All rights reserved.
+//
+
+import UIKit
+
+class Q1ButtonCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
